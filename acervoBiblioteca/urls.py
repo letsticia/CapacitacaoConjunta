f726d9912f6623e8e02cc2c5af5a46828bedbcee
letsticia <leti.morais48@gmail.com>
2024-03-15 20:12:36 -0300

    fix: double includes

--- acervoBiblioteca/urls.py
+++ acervoBiblioteca/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('biblioteca.urls')),
-    path('index/', include('biblioteca.urls')) # garantindo que o index apareça das duas formas
 ]
