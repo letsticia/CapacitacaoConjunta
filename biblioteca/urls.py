6bfc55ccff57d4b6c07c48d1aef2604515d507eb
letsticia <leti.morais48@gmail.com>
2024-03-23 23:21:30 -0300

    refactor: added logins navbar urls

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index, cadastro
+from .views import index, cadastro, login, login_funcionario
 
 urlpatterns = [
     path('', index, name='index'),
-    path('cadastro', cadastro, name='cadastro')
+    path('cadastro', cadastro, name='cadastro'),
+    path('login', login, name='login'),
+    path('login_funcionario', login_funcionario, name='login_funcionario'),
 ]
